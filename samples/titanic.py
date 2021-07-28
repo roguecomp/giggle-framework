2c6f1a4389ac5bba94715a346e0c781dafbdd577
roguecomp <roguecomp001@gmail.com>
2021-07-28 23:09:06 +1000

    changed branches for cleaner workflow

--- samples/titanic.py
+++ samples/titanic.py
@@ -1,9 +1,14 @@
-from gframe import *
+from gframe import gframe
+import pandas as pd
+import numpy as np
+import numba
 
 # %%
+@numba.jit(forceobj=True, parallel=True)
 def custom(predicted, actual):
-    guessed_correctly = sum(np.array(np.round(predicted)) == np.array(actual))
+    guessed_correctly = np.sum(np.equal(np.array(np.round(predicted)), np.array(actual)))
     return "{}/{} : {:.5f}".format(guessed_correctly, len(predicted), guessed_correctly / len(actual))
+temp = custom([0], [0])
 # %%
 # from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
@@ -13,7 +18,7 @@ df=pd.read_csv("../data/train.csv", low_memory=False)
 from sklearn.metrics import mean_squared_error
 gframe(df) \
     .fillna(median=True, categories = ['Embarked', 'Sex'], drop = False) \
-    .train(models=[LinearRegression(), RandomForestClassifier()],
+    .train(models=[LinearRegression(), RandomForestClassifier(n_jobs=8)],
            Y = 'Survived',
            x = ['Age', 'Pclass', 'male', 'female', 'S', 'C', 'Q'],
            metrics = [mean_squared_error, custom])
