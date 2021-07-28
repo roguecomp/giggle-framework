454129203ebd278657909d32d17b5aee4eb2b2d4
roguecomp <roguecomp001@gmail.com>
2021-07-28 18:17:40 +1000

    Added colours

--- samples/titanic.py
+++ samples/titanic.py
@@ -1,14 +1,18 @@
-from gframe import *
+#from gframe import *
+from gframe import gframe
+import numpy as np
+import pandas as pd
 
 # %%
-def custom(x, y):
-    return "{}/{} : {}".format(sum(np.array(x) == np.array(y)), len(x), sum(np.array(x) == np.array(y)) / len(x))
+def custom(predicted, actual):
+    guessed_correctly = sum(np.array(np.round(predicted)) == np.array(actual))
+    return "{}/{} : {:.5f}".format(guessed_correctly, len(predicted), guessed_correctly / len(predicted))
 # %%
 # from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 
-df=pd.read_csv("data/train.csv", low_memory=False)
+df=pd.read_csv("../data/train.csv", low_memory=False)
 from sklearn.metrics import mean_squared_error
 gframe(df) \
     .fillna(median=True, categories = ['Embarked', 'Sex'], drop = False) \
